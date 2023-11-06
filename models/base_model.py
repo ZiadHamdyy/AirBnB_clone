236e992f7b2a16a98f51542f2a3e89128bed602f
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-11-06 18:21:02 +0200

    task 3

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+"""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """BaseModel class"""
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        dictionary = self.__dict__
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
