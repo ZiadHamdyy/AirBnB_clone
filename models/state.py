f2ec50978e98c578cc1bedfd6d864560bb41d90e
esraaashraf023 <esraaashraf780567@gmail.com>
2023-11-10 11:36:10 -0800

    add file state

---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    This module contains State class that inherits from BaseModel.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    BaseModel class.
+    """
+
+    name = ""
