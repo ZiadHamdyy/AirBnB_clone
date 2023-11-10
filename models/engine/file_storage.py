fbc6aa62489b3565c13cc1567549511f21b49c52
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-11-10 16:37:19 +0200

    finaly task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import json
+from models.base_model import BaseModel
 
 class FileStorage:
     """FileStorage class"""
@@ -17,21 +18,21 @@ class FileStorage:
 
     def save(self):
         serialized = {}
-        for key, obj in serialized:
-            serialized[key] = obj.to_dect()
+        for key, obj in self.__objects.items():
+            serialized[key] = obj.to_dict()
 
-        with open(self.__file_path, 'w') as file:
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(serialized, file)
+
     def reload(self):
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
                 loaded = json.load(file)
-
-            for key, obj_dict in loaded.item():
+            
+            for key, obj_dict in loaded.items():
                 class_name, obj_id = key.split('.')
                 class_instance = globals()[class_name]
-                obj = class_instance(obj_dict)
+                obj = class_instance(**obj_dict)
                 self.__objects[key] = obj
-
         except FileNotFoundError:
             pass
