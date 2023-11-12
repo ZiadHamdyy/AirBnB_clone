a294169f8c3c759ecea21fc995949b74ee0efe88
ZiadHamdyy <ziadhamdy667@gmail.com>
2023-11-12 20:44:40 +0200

    add files

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,6 @@
 """create a unique FileStorage instance for the application."""
 
 from models.engine.file_storage import FileStorage
-from models.user import User
 
 storage = FileStorage()
 storage.reload()
